f77df60621b70ff3d9edbc942152fcb2ccf21b91
Åke Forslund <ake.forslund@gmail.com>
2018-02-20 11:14:50 +0100

    Move api and display_manager from enclosure client
    The api and displaymanager should be generic and disconnected from the
    specific enclosure.

--- mycroft/client/enclosure/display_manager.py
+++ mycroft/client/enclosure/display_manager.py
@@ -15,31 +15,13 @@
 
 """ DisplayManager
 
-This module provides basic "state" for the visual representation associated
-with this Mycroft instance.  The current states are:
-   ActiveSkill - The skill that last interacted with the display via the
-                 Enclosure API.
-
-Currently, a wakeword sets the ActiveSkill to "wakeword", which will auto
-clear after 10 seconds.
-
-A skill is set to Active when it matches an intent, outputs audio, or
-changes the display via the EnclosureAPI()
-
-A skill is automatically cleared from Active two seconds after audio
-output is spoken, or 2 seconds after resetting the display.
-
-So it is common to have '' as the active skill.
+This module is depreciated and only remains for backwards compatibility,
+use mycroft.enclosure.display_manager.
 """
-
-import json
-from threading import Thread, Timer
-
-import os
-
-from mycroft.messagebus.client.ws import WebsocketClient
-from mycroft.util import get_ipc_directory
+from mycroft.enclosure import display_manager
 from mycroft.util.log import LOG
+LOG.info('mycroft.client.enclosure.display_manager is depeciated '
+         'use mycroft.enclosure.display_manager')
 
 
 def _write_data(dictionary):
@@ -48,41 +30,7 @@ def _write_data(dictionary):
     Args:
         dictionary (dict): information to place in the 'disp_info' file
     """
-
-    managerIPCDir = os.path.join(get_ipc_directory(), "managers")
-    # change read/write permissions based on if file exists or not
-    path = os.path.join(managerIPCDir, "disp_info")
-    permission = "r+" if os.path.isfile(path) else "w+"
-
-    if permission == "w+" and os.path.isdir(managerIPCDir) is False:
-        os.makedirs(managerIPCDir)
-        os.chmod(managerIPCDir, 0o777)
-
-    try:
-        with open(path, permission) as dispFile:
-
-            # check if file is empty
-            if os.stat(str(dispFile.name)).st_size != 0:
-                data = json.load(dispFile)
-
-            else:
-                data = {}
-                LOG.info("Display Manager is creating " + dispFile.name)
-
-            for key in dictionary:
-                data[key] = dictionary[key]
-
-            dispFile.seek(0)
-            dispFile.write(json.dumps(data))
-            dispFile.truncate()
-
-        os.chmod(path, 0o777)
-
-    except Exception as e:
-        LOG.error(e)
-        LOG.error("Error found in display manager file, deleting...")
-        os.remove(path)
-        _write_data(dictionary)
+    return display_manager._write_data(dictionary)
 
 
 def _read_data():
@@ -90,27 +38,7 @@ def _read_data():
     Returns:
         dict: loaded state information
     """
-    managerIPCDir = os.path.join(get_ipc_directory(), "managers")
-
-    path = os.path.join(managerIPCDir, "disp_info")
-    permission = "r" if os.path.isfile(path) else "w+"
-
-    if permission == "w+" and os.path.isdir(managerIPCDir) is False:
-        os.makedirs(managerIPCDir)
-
-    data = {}
-    try:
-        with open(path, permission) as dispFile:
-
-            if os.stat(str(dispFile.name)).st_size != 0:
-                data = json.load(dispFile)
-
-    except Exception as e:
-        LOG.error(e)
-        os.remove(path)
-        _read_data()
-
-    return data
+    return display_manager._read_data()
 
 
 def set_active(skill_name):
@@ -118,7 +46,7 @@ def set_active(skill_name):
     Args:
         string: skill_name
     """
-    _write_data({"active_skill": skill_name})
+    return display_manager.set_active(skill_name)
 
 
 def get_active():
@@ -126,57 +54,14 @@ def get_active():
     Returns:
         string: The active skill's name
     """
-    data = _read_data()
-    active_skill = ""
-
-    if "active_skill" in data:
-        active_skill = data["active_skill"]
-
-    return active_skill
+    return display_manager.get_active()
 
 
 def remove_active():
     """ Clears the active skill """
-    LOG.debug("Removing active skill...")
-    _write_data({"active_skill": ""})
+    return display_manager.remove_active()
 
 
 def initiate_display_manager_ws():
     """ Initiates the web sockets on the display_manager """
-    LOG.info("Initiating display manager websocket")
-
-    # Should remove needs to be an object so it can be referenced in functions
-    # [https://stackoverflow.com/questions/986006/how-do-i-pass-a-variable-by-reference]
-    should_remove = [True]
-
-    def check_flag(flag):
-        if flag[0] is True:
-            remove_active()
-
-    def set_delay(event=None):
-        should_remove[0] = True
-        Timer(2, check_flag, [should_remove]).start()
-
-    def set_remove_flag(event=None):
-        should_remove[0] = False
-
-    def connect():
-        ws.run_forever()
-
-    def remove_wake_word():
-        data = _read_data()
-        if "active_skill" in data and data["active_skill"] == "wakeword":
-            remove_active()
-
-    def set_wakeword_skill(event=None):
-        set_active("wakeword")
-        Timer(10, remove_wake_word).start()
-
-    ws = WebsocketClient()
-    ws.on('recognizer_loop:audio_output_end', set_delay)
-    ws.on('recognizer_loop:audio_output_start', set_remove_flag)
-    ws.on('recognizer_loop:record_begin', set_wakeword_skill)
-
-    event_thread = Thread(target=connect)
-    event_thread.setDaemon(True)
-    event_thread.start()
+    display_manager.initiate_display_manager_ws()
