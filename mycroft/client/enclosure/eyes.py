b245001bb59c179214e50648906b83315eb4642e
Åke Forslund <ake.forslund@gmail.com>
2018-02-25 20:15:59 +0100

    Add queue system to eyes

--- mycroft/client/enclosure/eyes.py
+++ mycroft/client/enclosure/eyes.py
@@ -12,8 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from threading import Thread, Lock
+import time
 
-class EnclosureEyes:
+
+class EnclosureEyes(Thread):
     """
     Listens to enclosure commands for Mycroft's Eyes.
 
@@ -24,6 +27,14 @@ class EnclosureEyes:
         self.ws = ws
         self.writer = writer
         self.__init_events()
+
+        # Setup clearable/inspectable queue
+        self.queue_lock = Lock()
+        self.__queue = []
+
+        # Start queue handling thread
+        self.daemon = True
+        self.start()
 
     def __init_events(self):
         self.ws.on('enclosure.eyes.on', self.on)
@@ -40,25 +51,47 @@ class EnclosureEyes:
         self.ws.on('enclosure.eyes.setpixel', self.set_pixel)
         self.ws.on('enclosure.eyes.fill', self.fill)
 
+    def queue_clear(self):
+        with self.queue_lock:
+            self.queue = []
+
+    def queue_up(self, command, time=None, owner=None):
+        with self.queue_lock:
+            self.queue.append((command, time, owner))
+
+    def run(self):
+        while self.running:
+            with self.queue_lock:
+                if len(self.queue) > 0:
+                    command, timeout, owner = self.queue.pop()
+                    self.writer.write(command)
+                    time.sleep(timeout or 0.1)
+                else:
+                    time.sleep(0.1)
+
+    def clear_queue(self):
+        with self.queue_lock:
+            self.queue = []
+
     def on(self, event=None):
-        self.writer.write("eyes.on")
+        self.queue_up("eyes.on")
 
     def off(self, event=None):
-        self.writer.write("eyes.off")
+        self.queue_up("eyes.off")
 
     def blink(self, event=None):
         side = "b"
         if event and event.data:
             side = event.data.get("side", side)
-        self.writer.write("eyes.blink=" + side)
+        self.queue_up("eyes.blink=" + side)
 
     def narrow(self, event=None):
-        self.writer.write("eyes.narrow")
+        self.queue_up("eyes.narrow")
 
     def look(self, event=None):
         if event and event.data:
             side = event.data.get("side", "")
-            self.writer.write("eyes.look=" + side)
+            self.queue_up("eyes.look=" + side)
 
     def color(self, event=None):
         r, g, b = 255, 255, 255
@@ -67,7 +100,7 @@ class EnclosureEyes:
             g = int(event.data.get("g", g))
             b = int(event.data.get("b", b))
         color = (r * 65536) + (g * 256) + b
-        self.writer.write("eyes.color=" + str(color))
+        self.queue_up("eyes.color=" + str(color))
 
     def set_pixel(self, event=None):
         idx = 0
@@ -78,35 +111,35 @@ class EnclosureEyes:
             g = int(event.data.get("g", g))
             b = int(event.data.get("b", b))
         color = (r * 65536) + (g * 256) + b
-        self.writer.write("eyes.set=" + str(idx) + "," + str(color))
+        self.queue_up("eyes.set=" + str(idx) + "," + str(color))
 
     def fill(self, event=None):
         amount = 0
         if event and event.data:
             percent = int(event.data.get("percentage", 0))
             amount = int(round(23.0 * percent / 100.0))
-        self.writer.write("eyes.fill=" + str(amount))
+        self.queue_up("eyes.fill=" + str(amount))
 
     def brightness(self, event=None):
         level = 30
         if event and event.data:
             level = event.data.get("level", level)
-        self.writer.write("eyes.level=" + str(level))
+        self.queue_up("eyes.level=" + str(level))
 
     def volume(self, event=None):
         volume = 4
         if event and event.data:
             volume = event.data.get("volume", volume)
-        self.writer.write("eyes.volume=" + str(volume))
+        self.queue_up("eyes.volume=" + str(volume))
 
     def reset(self, event=None):
-        self.writer.write("eyes.reset")
+        self.queue_up("eyes.reset")
 
     def spin(self, event=None):
-        self.writer.write("eyes.spin")
+        self.queue_up("eyes.spin")
 
     def timed_spin(self, event=None):
         length = 5000
         if event and event.data:
             length = event.data.get("length", length)
-        self.writer.write("eyes.spin=" + str(length))
+        self.queue_up("eyes.spin=" + str(length))
